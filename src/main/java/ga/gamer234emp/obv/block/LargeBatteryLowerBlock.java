bd0cef0fc87ced971b4de619e9cd6fa390640cec
declan234e <gamer234e@gmail.com>
2022-03-14 18:57:07 +0000

    final push from 1.16.5

--- src/main/java/ga/gamer234emp/obv/block/LargeBatteryLowerBlock.java
+++ src/main/java/ga/gamer234emp/obv/block/LargeBatteryLowerBlock.java
@@ -172,7 +172,7 @@ public class LargeBatteryLowerBlock extends OblivionModElements.ModElement {
 			int x = pos.getX();
 			int y = pos.getY();
 			int z = pos.getZ();
-			world.getPendingBlockTicks().scheduleTick(pos, this, 10);
+			world.getPendingBlockTicks().scheduleTick(pos, this, 5);
 
 			LargeBatteryPlacedProcedure.executeProcedure(Stream
 					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
@@ -191,7 +191,7 @@ public class LargeBatteryLowerBlock extends OblivionModElements.ModElement {
 					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
 							new AbstractMap.SimpleEntry<>("z", z))
 					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
-			world.getPendingBlockTicks().scheduleTick(pos, this, 10);
+			world.getPendingBlockTicks().scheduleTick(pos, this, 5);
 		}
 
 		@Override
